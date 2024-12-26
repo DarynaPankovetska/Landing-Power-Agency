cdf8509ec13a4ecb18a5273d59cd8a9c5af7456b
DarynaPankovetska <darynapankovetska@gmail.com>
2024-12-26 09:42:18 +0200

    feat: added breakpoints

--- src/js/script.js
+++ src/js/script.js
@@ -2,6 +2,7 @@
 
 const swiperTwo = new Swiper('.swiper_2', {
 	slidesPerView: 5,
+  spaceBetween: 10,
 });
 
 const swiperOne = new Swiper('.swiper_1', {
@@ -16,4 +17,30 @@ const swiperThree = new Swiper('.swiper_3', {
 	autoplay: {
 		delay: 3000,
 	},
+	breakpoints: {
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 20,
+        },
+        640: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 4,
+          spaceBetween: 40,
+        },
+        1024: {
+          slidesPerView: 5,
+          spaceBetween: 50,
+        },
+      },
+});
+
+const menuButton = document.querySelector('.menu__button');
+const nav = document.querySelector('.nav');
+
+menuButton.addEventListener('click', function () {
+	menuButton.classList.toggle('active');
+  nav.classList.toggle('active');
 });
